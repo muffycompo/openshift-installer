97e95f37380e8199e9a65c7ca663d64f74b59349
Ryan Leaf <rjleaf@tigre.meiguoren.res.wpi.net>
2014-06-12 12:04:32 -0400

    updated support for rhel

--- install.sh
+++ install.sh
@@ -116,6 +116,9 @@ redhat()
 
 	if [[ releasenum == "6.5" ]]; then
 		echo "Installing on RHEL 6.5"
+		yum -y install http://mirror.metrocast.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
+		yum -y update
+		yum -y install ruby193
 	elif [[ releasenum == "7" ]]; then
 		echo "Installing on RHEL 7"
 	else
