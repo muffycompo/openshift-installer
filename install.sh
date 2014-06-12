00bd7ce487b9b64988f0bfb95c507bd86d6d3be3
Ryan Leaf <rjleaf@tigre.meiguoren.res.wpi.net>
2014-06-12 14:21:25 -0400

    making minor changse to the script.

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ redhat()
 		echo "Installing on RHEL 6.5"
 		yum -y install http://mirror.metrocast.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
 		yum -y update
-		yum -y install ruby193
+		yum -y install ruby193 ruby puppet augeas bind httpd-tools curl
 	elif [[ $releasenum == *"7"* ]]; then
 		echo "Installing on RHEL 7"
 	else
